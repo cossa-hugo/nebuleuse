35a99bf3e9c844802e63b022cbbc697f84a735e2
Hugo <hugo.cossa@hotmail.fr>
2023-02-25 16:41:27 +0100

    init api skeletons

---
+++ src/main/java/com/cossa/app/nebuleuse/NebuleuseApplication.java
@@ -0,0 +1,13 @@
+package com.cossa.app.nebuleuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebuleuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebuleuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cossa/app/nebuleuse/NebuleuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cossa.app.nebuleuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebuleuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
